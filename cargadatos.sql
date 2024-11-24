d635898e668ee1defed392502c24702055d874de
Tomoko Kuroki <yanfeicuenta@gmail.com>
2024-11-23 23:08:23 -0300

    si

--- cargadatos.sql
+++ cargadatos.sql
@@ -1,3 +1,44 @@
+-- Drop all tables
+BEGIN
+    FOR t IN (SELECT table_name FROM user_tables) LOOP
+        EXECUTE IMMEDIATE 'DROP TABLE ' || t.table_name || ' CASCADE CONSTRAINTS';
+    END LOOP;
+END;
+/
+
+-- Drop all sequences
+BEGIN
+    FOR s IN (SELECT sequence_name FROM user_sequences) LOOP
+        EXECUTE IMMEDIATE 'DROP SEQUENCE ' || s.sequence_name;
+    END LOOP;
+END;
+/
+
+-- Drop all procedures
+BEGIN
+    FOR p IN (SELECT object_name FROM user_objects WHERE object_type = 'PROCEDURE') LOOP
+        EXECUTE IMMEDIATE 'DROP PROCEDURE ' || p.object_name;
+    END LOOP;
+END;
+/
+
+-- Drop all functions
+BEGIN
+    FOR f IN (SELECT object_name FROM user_objects WHERE object_type = 'FUNCTION') LOOP
+        EXECUTE IMMEDIATE 'DROP FUNCTION ' || f.object_name;
+    END LOOP;
+END;
+/
+
+-- Drop all triggers
+BEGIN
+    FOR tr IN (SELECT trigger_name FROM user_triggers) LOOP
+        EXECUTE IMMEDIATE 'DROP TRIGGER ' || tr.trigger_name;
+    END LOOP;
+END;
+/
+
+
 begin
 	AGREGAR_REGIONES('LORSON PAIS');
     AGREGAR_REGIONES('PAIS naiko');
